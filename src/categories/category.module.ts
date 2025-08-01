65d1bc18958fe6e1c23e691656ebe65e9579dbe8
franco <franco.scioli.13@gmail.com>
2025-08-01 17:12:45 -0300

    S3 module added category provider

--- src/categories/category.module.ts
+++ src/categories/category.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
+import { S3Module } from 'src/aws-s3/s3.module';
 
 @Module({
-  providers: [CategoryService],
+  providers: [CategoryService, S3Module],
   controllers: [CategoryController]
 })
 export class CategoryModule {}
