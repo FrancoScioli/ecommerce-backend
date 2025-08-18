afa6b52a9361fd923c14349fdf412fe1f210535a
franco <franco.scioli.13@gmail.com>
2025-08-18 15:18:16 -0300

    fix modulo S3 imports

--- src/categories/category.module.ts
+++ src/categories/category.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
-import { S3Module } from 'src/aws-s3/s3.module';
+import { S3Module } from '../aws-s3/s3.module';
 
 @Module({
-  providers: [CategoryService, S3Module],
+  imports: [S3Module],
+  providers: [CategoryService],
   controllers: [CategoryController]
 })
 export class CategoryModule {}
